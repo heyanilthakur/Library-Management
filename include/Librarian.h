281d30a612d0b88cd1d034ac304393c22be914cc
anilxthakur <anilthakur.official@hotmail.com>
2023-08-11 13:34:27 +0530

    shifted encapsulated fun() to Librarian.h

--- include/Librarian.h
+++ include/Librarian.h
@@ -1,256 +1,312 @@
 #include "LibrarianDef.h"
-#include "date.h"
 #include <fstream>
 #include <iostream>
 #include <list>
 #include <string>
 using namespace std;
+int fx = 1;
 
-void BookList::addBook(string serial, string bookname, string author,
-                       string publishdate) {
+// void BookList::addBook(string serial, string bookname, string author,
+//                        string publishdate) {
+//   try {
+//     Book newBook = {serial, bookname, author, publishdate, "",
+//                     "",     "",       true,   "Available"};
 
-  Book newBook = {serial, bookname, author, publishdate, "",
-                  "",     "",       true,   "Available"};
+//     books.push_back(newBook);
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in addBook: " << e.what() << endl;
+//   }
+// }
 
-  books.push_back(newBook);
-}
+// void BookList::deleteBook(string serial) {
+//   try {
 
-void BookList::deleteBook(string serial) {
+//     for (auto it = books.begin(); it != books.end(); ++it) {
+//       if (it->serial == serial) {
 
-  for (auto it = books.begin(); it != books.end(); ++it) {
-    if (it->serial == serial) {
+//         books.erase(it);
+//         break;
+//       }
+//     }
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in deleteBook: " << e.what() << endl;
+//   }
+// }
 
-      books.erase(it);
-      break;
-    }
-  }
-}
+// void BookList::writeToFile(string filename) {
+//   try {
+//     fstream outFile(filename, ios::out);
+//     if (outFile.is_open()) {
+//       for (const auto &book : books) {
+//         outFile << book.serial << "," << book.bookname << "," << book.author
+//                 << "," << book.publishdate << "," << book.borrowerID << ","
+//                 << book.borrowDate << "," << book.dueDate << ","
+//                 << book.returned << "," << book.status << "\n";
+//       }
+//       outFile.close();
+//       cout << "Data written to " << filename << " successfully.\n";
+//     } else {
+//       cerr << "Unable to open file: " << filename << "\n";
+//     }
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in writeToFile: " << e.what() << endl;
+//   }
+// }
 
-void BookList::writeToFile(string filename) {
-  fstream outFile(filename, ios::out);
-  if (outFile.is_open()) {
-    for (const auto &book : books) {
-      outFile << book.serial << "," << book.bookname << "," << book.author
-              << "," << book.publishdate << "," << book.borrowerID << ","
-              << book.borrowDate << "," << book.dueDate << "," << book.returned
-              << "," << book.status << "\n";
-    }
-    outFile.close();
-    cout << "Data written to " << filename << " successfully.\n";
-  } else {
-    cerr << "Unable to open file: " << filename << "\n";
-  }
-}
+// void BookList::modifyBook(string serial, string bookname, string author,
+//                           string publishdate) {
+//   try {
 
-void BookList::modifyBook(string serial, string bookname, string author,
-                          string publishdate) {
+//     for (auto &book : books) {
+//       if (book.serial == serial) {
 
-  for (auto &book : books) {
-    if (book.serial == serial) {
+//         if (!bookname.empty()) {
+//           book.bookname = bookname;
+//         }
+//         if (!author.empty()) {
+//           book.author = author;
+//         }
+//         if (!publishdate.empty()) {
 
-      if (!bookname.empty()) {
-        book.bookname = bookname;
-      }
-      if (!author.empty()) {
-        book.author = author;
-      }
-      if (!publishdate.empty()) {
+//           if (isValidDate(publishdate)) {
+//             book.publishdate = publishdate;
+//           } else {
 
-        if (isValidDate(publishdate)) {
-          book.publishdate = publishdate;
-        } else {
+//             std::cout << "Invalid publish date: " << publishdate << "\n";
+//           }
+//         }
+//         break;
+//       }
+//     }
+//   } catch (const invalid_argument &e) {
+//     cout << "Error Found in modifyBook: " << e.what() << endl;
+//   }
+// }
+// void BookList::borrowBook(string serial, string borrowerID, string
+// borrowDate,
+//                           string dueDate) {
+//   try {
+//     for (auto &book : books) {
+//       if (book.serial == serial && book.borrowerID.empty()) {
+//         book.borrowerID = borrowerID;
+//         book.borrowDate = borrowDate;
+//         book.dueDate = dueDate;
+//         book.returned = false;
+//         book.status = "Borrowed";
+//         cout << "Book borrowed successfully!" << endl;
+//         return;
+//       }
+//     }
+//     cout << "Book not available for borrowing." << endl;
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in borrowBook: " << e.what() << endl;
+//   }
+// }
+// void BookList::returnBook(string serial) {
+//   try {
+//     for (auto &book : books) {
+//       if (book.serial == serial && !book.borrowerID.empty() &&
+//       !book.returned) {
+//         book.returned = true;
 
-          std::cout << "Invalid publish date: " << publishdate << "\n";
-        }
-      }
-      break;
-    }
-  }
-}
-void BookList::borrowBook(string serial, string borrowerID, string borrowDate,
-                          string dueDate) {
-  for (auto &book : books) {
-    if (book.serial == serial && book.borrowerID.empty()) {
-      book.borrowerID = borrowerID;
-      book.borrowDate = borrowDate;
-      book.dueDate = dueDate;
-      book.returned = false;
-      book.status = "Borrowed";
-      cout << "Book borrowed successfully!" << endl;
-      return;
-    }
-  }
-  cout << "Book not available for borrowing." << endl;
-}
-void BookList::returnBook(string serial) {
-  for (auto &book : books) {
-    if (book.serial == serial && !book.borrowerID.empty() && !book.returned) {
-      book.returned = true;
+//         if (isValidDate(book.dueDate)) {
+//           time_t now = time(nullptr);
+//           tm dueTm = {};
+//           istringstream(book.dueDate) >> get_time(&dueTm, "%Y-%m-%d");
+//           time_t dueTime = mktime(&dueTm);
 
-      if (isValidDate(book.dueDate)) {
-        time_t now = time(nullptr);
-        tm dueTm = {};
-        istringstream(book.dueDate) >> get_time(&dueTm, "%Y-%m-%d");
-        time_t dueTime = mktime(&dueTm);
+//           double secondsLate = difftime(now, dueTime);
+//           double daysLate = secondsLate / (60 * 60 * 24);
 
-        double secondsLate = difftime(now, dueTime);
-        double daysLate = secondsLate / (60 * 60 * 24);
+//           if (daysLate > 0) {
+//             double finePerDay = 5.0;
+//             double fineAmount = daysLate * finePerDay;
+//             cout << "Fine calculated: Rs." << fineAmount << endl;
+//           }
+//         }
 
-        if (daysLate > 0) {
-          double finePerDay = 5.0;
-          double fineAmount = daysLate * finePerDay;
-          cout << "Fine calculated: Rs." << fineAmount << endl;
-        }
-      }
+//         book.status = "Available";
+//         book.borrowerID = "";
+//         book.borrowDate = "";
+//         book.dueDate = "";
 
-      book.status = "Available";
-      book.borrowerID = "";
-      book.borrowDate = "";
-      book.dueDate = "";
+//         cout << "Book returned successfully!" << endl;
+//         return;
+//       }
+//     }
+//     cout << "Book not available for returning." << endl;
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in returnBook: " << e.what() << endl;
+//   }
+// }
+// void BookList::listBorrowedBooksByBorrowerID(string borrowerID) {
+//   try {
+//     bool found = false;
 
-      cout << "Book returned successfully!" << endl;
-      return;
-    }
-  }
-  cout << "Book not available for returning." << endl;
-}
-void BookList::listBorrowedBooksByBorrowerID(string borrowerID) {
-  bool found = false;
+//     cout << "Books borrowed by Borrower ID " << borrowerID << ":" << endl;
 
-  cout << "Books borrowed by Borrower ID " << borrowerID << ":" << endl;
+//     for (const auto &book : books) {
+//       if (book.borrowerID == borrowerID && !book.returned) {
+//         found = true;
+//         cout << "Serial: " << book.serial << endl;
+//         cout << "Book Name: " << book.bookname << endl;
+//         cout << "Author: " << book.author << endl;
+//         cout << "Publish Date: " << book.publishdate << endl;
+//         cout << "Borrow Date: " << book.borrowDate << endl;
+//         cout << "Due Date: " << book.dueDate << endl;
+//         cout << "Status: " << book.status << endl;
+//         cout << "-------------------------------" << endl;
+//       }
+//     }
 
-  for (const auto &book : books) {
-    if (book.borrowerID == borrowerID && !book.returned) {
-      found = true;
-      cout << "Serial: " << book.serial << endl;
-      cout << "Book Name: " << book.bookname << endl;
-      cout << "Author: " << book.author << endl;
-      cout << "Publish Date: " << book.publishdate << endl;
-      cout << "Borrow Date: " << book.borrowDate << endl;
-      cout << "Due Date: " << book.dueDate << endl;
-      cout << "Status: " << book.status << endl;
-      cout << "-------------------------------" << endl;
-    }
-  }
-
-  if (!found) {
-    cout << "No borrowed books found for Borrower ID " << borrowerID << endl;
-  }
-}
+//     if (!found) {
+//       cout << "No borrowed books found for Borrower ID " << borrowerID <<
+//       endl;
+//     }
+//   } catch (const invalid_argument &e) {
+//     cout << "Error found in listBorrowedBooksByBorrowerID: " << e.what()
+//          << endl;
+//   }
+// }
 void addBooks(BookList &books) {
-  string serial, bookname, author, publishdate;
+  try {
+    string serial, bookname, author, publishdate;
 
-  cout << "Enter the following details: " << endl;
+    cout << "Enter the following details: " << endl;
 
-  while (true) {
-    cout << "BOOK SERIAL (enter 'q' to quit): ";
+    while (true) {
+      cout << "BOOK SERIAL (enter 'q' to quit): ";
+      getline(cin, serial);
+
+      if (serial == "q")
+        break;
+
+      cout << "BOOKNAME: ";
+      getline(cin, bookname);
+
+      cout << "AUTHOR: ";
+      getline(cin, author);
+
+      cout << "PUBLISH DATE(DD MM YYYY): ";
+      getline(cin, publishdate);
+
+      if (isValidDate(publishdate)) {
+        books.addBook(serial, bookname, author, publishdate);
+      } else {
+        cout << "Invalid publish date format." << endl;
+      }
+    }
+
+    books.writeToFile(
+        "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+    cout << "Books added successfully!" << endl;
+  } catch (const invalid_argument &e) {
+    cout << "Error found in addBooks: " << e.what() << endl;
+  }
+}
+
+void deleteBooks(BookList &books) {
+  try {
+    string serial;
+
+    cout << "Enter the following details: " << endl;
+
+    cout << "BOOK SERIAL: ";
     getline(cin, serial);
 
-    if (serial == "q")
-      break;
-
-    cout << "BOOKNAME: ";
-    getline(cin, bookname);
-
-    cout << "AUTHOR: ";
-    getline(cin, author);
-
-    cout << "PUBLISH DATE(DD MM YYYY): ";
-    getline(cin, publishdate);
-
-    if (isValidDate(publishdate)) {
-      books.addBook(serial, bookname, author, publishdate);
-    } else {
-      cout << "Invalid publish date format." << endl;
-    }
+    books.deleteBook(serial);
+    books.writeToFile(
+        "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+  } catch (const invalid_argument &e) {
+    cout << "Error found in deleteBooks: " << e.what() << endl;
   }
-
-  books.writeToFile(
-      "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
-  cout << "Books added successfully!" << endl;
-}
-
-void deleteBook(BookList &books) {
-  string serial;
-
-  cout << "Enter the following details: " << endl;
-
-  cout << "BOOK SERIAL: ";
-  getline(cin, serial);
-
-  books.deleteBook(serial);
-  books.writeToFile(
-      "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
 }
 void modifyBooks(BookList &books) {
-  std::string serial, bookname, author, publishdate;
+  try {
+    std::string serial, bookname, author, publishdate;
 
-  std::cout << "Enter the following details: " << std::endl;
+    std::cout << "Enter the following details: " << std::endl;
 
-  std::cout << "BOOK SERIAL: ";
-  getline(std::cin, serial);
+    std::cout << "BOOK SERIAL: ";
+    getline(std::cin, serial);
 
-  std::cout << "NEW BOOK NAME: ";
-  getline(std::cin, bookname);
+    std::cout << "NEW BOOK NAME: ";
+    getline(std::cin, bookname);
 
-  std::cout << "NEW AUTHOR NAME: ";
-  getline(std::cin, author);
+    std::cout << "NEW AUTHOR NAME: ";
+    getline(std::cin, author);
 
-  std::cout << "NEW PUBLISH DATE: ";
-  getline(std::cin, publishdate);
+    std::cout << "NEW PUBLISH DATE: ";
+    getline(std::cin, publishdate);
 
-  books.modifyBook(serial, bookname, author, publishdate);
-  books.writeToFile(
-      "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+    books.modifyBook(serial, bookname, author, publishdate);
+    books.writeToFile(
+        "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+  } catch (const invalid_argument &e) {
+    cout << "Error found in modifyBooks: " << e.what() << endl;
+  }
 }
 
 void borrowBooks(BookList &books) {
-  string serial, borrowerID, borrowDate, dueDate;
+  try {
+    string serial, borrowerID, borrowDate, dueDate;
 
-  cout << "Enter the following details: " << endl;
+    cout << "Enter the following details: " << endl;
 
-  cout << "BOOK SERIAL: ";
-  getline(cin, serial);
+    cout << "BOOK SERIAL: ";
+    getline(cin, serial);
 
-  cout << "BORROWER ID: ";
-  getline(cin, borrowerID);
+    cout << "BORROWER ID: ";
+    getline(cin, borrowerID);
 
-  cout << "BORROW DATE(DD MM YYYY): ";
-  getline(cin, borrowDate);
+    cout << "BORROW DATE(DD MM YYYY): ";
+    getline(cin, borrowDate);
 
-  cout << "DUE DATE(DD MM YYYY): ";
-  getline(cin, dueDate);
+    cout << "DUE DATE(DD MM YYYY): ";
+    getline(cin, dueDate);
 
-  if (isValidDate(borrowDate) && isValidDate(dueDate)) {
-    books.borrowBook(serial, borrowerID, borrowDate, dueDate);
-  } else {
-    cout << "Invalid date format." << endl;
+    if (isValidDate(borrowDate) && isValidDate(dueDate)) {
+      books.borrowBook(serial, borrowerID, borrowDate, dueDate);
+    } else {
+      cout << "Invalid date format." << endl;
+    }
+
+    cout << "Book borrowed successfully!" << endl;
+    books.writeToFile(
+        "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+  } catch (const invalid_argument &e) {
+    cout << "Error found in borrowBooks: " << e.what() << endl;
   }
-
-  cout << "Book borrowed successfully!" << endl;
-  books.writeToFile(
-      "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
 }
 
 void returnBooks(BookList &books) {
-  string serial;
+  try {
+    string serial;
 
-  cout << "Enter the following details: " << endl;
+    cout << "Enter the following details: " << endl;
 
-  cout << "BOOK SERIAL: ";
-  getline(cin, serial);
+    cout << "BOOK SERIAL: ";
+    getline(cin, serial);
 
-  books.returnBook(serial);
-  books.writeToFile(
-      "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+    books.returnBook(serial);
+    books.writeToFile(
+        "/Users/AN20449220/Desktop/Library-Management/csv/data.csv");
+  } catch (const invalid_argument &e) {
+    cout << "Error found in returnBooks: " << e.what() << endl;
+  }
 }
 void listBorrowedBooksByBorrowerIDs(BookList &books) {
-  string borrowerID;
+  try {
+    string borrowerID;
 
-  cout << "Enter the borrower ID to list borrowed books: ";
-  getline(cin, borrowerID);
+    cout << "Enter the borrower ID to list borrowed books: ";
+    getline(cin, borrowerID);
 
-  books.listBorrowedBooksByBorrowerID(borrowerID);
+    books.listBorrowedBooksByBorrowerID(borrowerID);
+  } catch (const invalid_argument &e) {
+    cout << "Error found in listBorrowedBooksByBorrowerIDs : " << e.what()
+         << endl;
+  }
 }
 
 BookList books;
@@ -297,25 +353,21 @@ void LibraryLogin() {
     int choice;
     do {
       cout << "-----------------LIBRARY MANAGEMENT-------------------" << endl;
-      cout << "|                  Select An Option :                  |"
+      cout << "|                  Select An Option :                |"
            << endl;
-      cout << "|               1. Add Books from Library :            |"
+      cout << "|               1. Add Books in Library :            |"
            << endl;
-      cout << "|               2. Delete Books from Library :         |"
+      cout << "|               2. Delete Books of Library :         |"
            << endl;
-      cout << "|               3. Modify Books from Library :         |"
+      cout << "|               3. Modify Books of Library :         |"
            << endl;
-      cout << "|               4. Add Boorrowers from Library :       |"
+      cout << "|               4. List Borrowers from Library :     |"
            << endl;
-      cout << "|               5. Delete Borrowers from Library :     |"
+      cout << "|               5. Borrow Boook from Library :       |"
            << endl;
-      cout << "|               6. Modify Borrowers from Library :     |"
+      cout << "|               6. Return Book from Library :        |"
            << endl;
-      cout << "|               7. Borrow Boook from Library :         |"
-           << endl;
-      cout << "|               8. Return Book from Library :          |"
-           << endl;
-      cout << "|               9. Exit :                              |"
+      cout << "|               7. Exit :                            |"
            << endl;
       cout << "-------------------------------------------------------" << endl;
 
@@ -328,27 +380,21 @@ void LibraryLogin() {
         addBooks(books);
         break;
       case 2:
-        deleteBook(books);
+        deleteBooks(books);
         break;
       case 3:
         modifyBooks(books);
         break;
       case 4:
-
-        break;
-      case 5:
-
-        break;
-      case 6:
         listBorrowedBooksByBorrowerIDs(books);
         break;
-      case 7:
+      case 5:
         borrowBooks(books);
         break;
-      case 8:
+      case 6:
         returnBooks(books);
         break;
-      case 9:
+      case 7:
         exit(0);
         break;
 
@@ -365,5 +411,15 @@ void LibraryLogin() {
         break;
       }
     } while (true);
+  } else {
+    fx++;
+    if (fx <= 3) {
+      cout << "Enter Correct Name/Password" << endl;
+      cout << endl;
+      LibraryLogin();
+    } else {
+      cout << endl;
+      cout << "Login Limit Reached...Exiting the Application" << endl;
+    }
   }
 }
--- include/LibrarianDef.h
+++ include/LibrarianDef.h
@@ -1,3 +1,5 @@
+#include<iostream>
+#include "date.h"
 #include <fstream>
 #include <list>
 #include <string>
@@ -24,21 +26,162 @@ private:
 public:
   // Method to add a new book at the end of the list
   void addBook(string serial, string bookname, string author,
-               string publishdate);
+                       string publishdate) {
+  try {
+    Book newBook = {serial, bookname, author, publishdate, "",
+                    "",     "",       true,   "Available"};
 
-  // Method to delete a book from the list based on its serial number
-  void deleteBook(string serial);
+    books.push_back(newBook);
+  } catch (const invalid_argument &e) {
+    cout << "Error found in addBook: " << e.what() << endl;
+  }
+}
 
-  void listBorrowedBooksByBorrowerID(string borrowerID);
+void deleteBook(string serial) {
+  try {
 
-  // Method to write the list to a file
-  void writeToFile(string filename);
+    for (auto it = books.begin(); it != books.end(); ++it) {
+      if (it->serial == serial) {
 
-  // Method to modify a book's data based on its serial number
-  void modifyBook(string serial, string bookname = "", string author = "",
-                  string publishdate = "");
+        books.erase(it);
+        break;
+      }
+    }
+  } catch (const invalid_argument &e) {
+    cout << "Error found in deleteBook: " << e.what() << endl;
+  }
+}
 
-  void borrowBook(string serial, string borrowerID, string borrowDate,
-                  string dueDate);
-  void returnBook(string serial);
+void writeToFile(string filename) {
+  try {
+    fstream outFile(filename, ios::out);
+    if (outFile.is_open()) {
+      for (const auto &book : books) {
+        outFile << book.serial << "," << book.bookname << "," << book.author
+                << "," << book.publishdate << "," << book.borrowerID << ","
+                << book.borrowDate << "," << book.dueDate << ","
+                << book.returned << "," << book.status << "\n";
+      }
+      outFile.close();
+      cout << "Data written to " << filename << " successfully.\n";
+    } else {
+      cerr << "Unable to open file: " << filename << "\n";
+    }
+  } catch (const invalid_argument &e) {
+    cout << "Error found in writeToFile: " << e.what() << endl;
+  }
+}
+
+void modifyBook(string serial, string bookname, string author,
+                          string publishdate) {
+  try {
+
+    for (auto &book : books) {
+      if (book.serial == serial) {
+
+        if (!bookname.empty()) {
+          book.bookname = bookname;
+        }
+        if (!author.empty()) {
+          book.author = author;
+        }
+        if (!publishdate.empty()) {
+
+          if (isValidDate(publishdate)) {
+            book.publishdate = publishdate;
+          } else {
+
+            std::cout << "Invalid publish date: " << publishdate << "\n";
+          }
+        }
+        break;
+      }
+    }
+  } catch (const invalid_argument &e) {
+    cout << "Error Found in modifyBook: " << e.what() << endl;
+  }
+}
+void borrowBook(string serial, string borrowerID, string borrowDate,
+                          string dueDate) {
+  try {
+    for (auto &book : books) {
+      if (book.serial == serial && book.borrowerID.empty()) {
+        book.borrowerID = borrowerID;
+        book.borrowDate = borrowDate;
+        book.dueDate = dueDate;
+        book.returned = false;
+        book.status = "Borrowed";
+        cout << "Book borrowed successfully!" << endl;
+        return;
+      }
+    }
+    cout << "Book not available for borrowing." << endl;
+  } catch (const invalid_argument &e) {
+    cout << "Error found in borrowBook: " << e.what() << endl;
+  }
+}
+void returnBook(string serial) {
+  try {
+    for (auto &book : books) {
+      if (book.serial == serial && !book.borrowerID.empty() && !book.returned) {
+        book.returned = true;
+
+        if (isValidDate(book.dueDate)) {
+          time_t now = time(nullptr);
+          tm dueTm = {};
+          istringstream(book.dueDate) >> get_time(&dueTm, "%Y-%m-%d");
+          time_t dueTime = mktime(&dueTm);
+
+          double secondsLate = difftime(now, dueTime);
+          double daysLate = secondsLate / (60 * 60 * 24);
+
+          if (daysLate > 0) {
+            double finePerDay = 5.0;
+            double fineAmount = daysLate * finePerDay;
+            cout << "Fine calculated: Rs." << fineAmount << endl;
+          }
+        }
+
+        book.status = "Available";
+        book.borrowerID = "";
+        book.borrowDate = "";
+        book.dueDate = "";
+
+        cout << "Book returned successfully!" << endl;
+        return;
+      }
+    }
+    cout << "Book not available for returning." << endl;
+  } catch (const invalid_argument &e) {
+    cout << "Error found in returnBook: " << e.what() << endl;
+  }
+}
+void listBorrowedBooksByBorrowerID(string borrowerID) {
+  try {
+    bool found = false;
+
+    cout << "Books borrowed by Borrower ID " << borrowerID << ":" << endl;
+
+    for (const auto &book : books) {
+      if (book.borrowerID == borrowerID && !book.returned) {
+        found = true;
+        cout << "Serial: " << book.serial << endl;
+        cout << "Book Name: " << book.bookname << endl;
+        cout << "Author: " << book.author << endl;
+        cout << "Publish Date: " << book.publishdate << endl;
+        cout << "Borrow Date: " << book.borrowDate << endl;
+        cout << "Due Date: " << book.dueDate << endl;
+        cout << "Status: " << book.status << endl;
+        cout << "-------------------------------" << endl;
+      }
+    }
+
+    if (!found) {
+      cout << "No borrowed books found for Borrower ID " << borrowerID << endl;
+    }
+  } catch (const invalid_argument &e) {
+    cout << "Error found in listBorrowedBooksByBorrowerID: " << e.what()
+         << endl;
+  }
+}
 };
